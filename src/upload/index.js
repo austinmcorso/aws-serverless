b29e4dce982a44219c89e9408c92daa832692193
grrizzly <austin.r.montoya@gmail.com>
2017-02-18 12:59:21 -0800

    updating upload logic

--- src/upload/index.js
+++ src/upload/index.js
@@ -1,13 +1,35 @@
-module.exports = function upload(event, context, callback) {
-  console.log('Received event:', JSON.stringify(event, null, 2));
+'use strict';
 
-  const done = (err, res) => callback(null, {
-    statusCode: err ? '400' : '200',
-    body: err ? err.message : JSON.stringify(res),
-    headers: {
-      'Content-Type': 'application/json',
-    },
-  });
+var path = require('path');
+var AWS = require('aws-sdk');
+var uuid = require('uuid/v4');
 
-  done(null, '');
+const S3_BUCKET_NAME = 'mipmapper';
+const S3_FOLDER_PREFIX = 'uploads';
+const jsonResponse = (statusCode, body) => ({
+  statusCode: statusCode,
+  body: JSON.stringify(body),
+  headers: 'Content-Type': 'application/json'
+});
+
+function getValidationErrors(event) {
+  // TODO: Make sure the image is a supported format
+  // TODO: Make sure the image does exceed the maximum upload size
+  return [];
+}
+
+function doUpload(contents, id, callback) {
+  const s3 = new AWS.S3Client();
+  var filename = path.join(S3_FOLDER_PREFIX, id) + path.extension()
+  s3.putObject({ Bucket: S3_BUCKET_NAME, Key: path.join(S3_FOLDER_PREFIX, filename), Body: contents }, callback);
+}
+
+module.exports = (event, context, callback) => {
+    const errors = getValidationErrors(event);
+    if (errors.length > 0) {
+      callback(null, jsonResponse(400, { errors: errors }));
+    }
+
+    const id = uuid();
+    doUpload(event.payload, id, () => jsonResponse(200, { id: id }));
 };
