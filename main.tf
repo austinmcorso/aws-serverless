debc46d68088572a41e70610453ca1ab1b2906e0
grrizzly <austin.r.montoya@gmail.com>
2017-02-26 16:26:23 -0800

    new arn, new day

--- main.tf
+++ main.tf
@@ -92,7 +92,7 @@ resource "aws_api_gateway_rest_api" "mipmapper_api" {
 resource "aws_api_gateway_resource" "upload_image_api_gateway_resource" {
   rest_api_id = "${aws_api_gateway_rest_api.mipmapper_api.id}"
   parent_id = "${aws_api_gateway_rest_api.mipmapper_api.root_resource_id}"
-  path_part = "upload"
+  path_part = "images"
 }
 
 resource "aws_api_gateway_method" "upload_image_api_gateway_method" {
@@ -106,9 +106,9 @@ resource "aws_api_gateway_integration" "upload_image_api_gateway_integration" {
   rest_api_id = "${aws_api_gateway_rest_api.mipmapper_api.id}"
   resource_id = "${aws_api_gateway_resource.upload_image_api_gateway_resource.id}"
   http_method = "${aws_api_gateway_method.upload_image_api_gateway_method.http_method}"
-  type = "HTTP"
+  type = "AWS"
   integration_http_method = "${aws_api_gateway_method.upload_image_api_gateway_method.http_method}"
-  uri = "https://lambda.${var.aws_region}.amazonaws.com/2015-03-31/functions/${aws_lambda_function.upload_image_lambda_function.arn}/invocations"
+  uri = "arn:aws:apigateway:${var.aws_region}:lambda:path/2015-03-31/functions/${aws_lambda_function.upload_image_lambda_function.arn}/invocations"
 
   request_templates {
    "image/png" = <<EOF
