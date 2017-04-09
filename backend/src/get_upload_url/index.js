9235cfcbefe03022e652aa65f6b778f936390413
grrizzly <austin.r.montoya@gmail.com>
2017-04-09 14:27:08 -0700

    fixing integration errors, adding validation

--- backend/src/get_upload_url/index.js
+++ backend/src/get_upload_url/index.js
@@ -4,16 +4,39 @@ const uuid = require('uuid/v4');
 
 const S3_BUCKET_NAME = 'mipmapper';
 const S3_FOLDER_PREFIX = 'images';
+const ALLOWED_IMAGE_TYPES = ['jpg', 'png'];
+const MAX_FILE_SIZE_BYTES = 5 * 1024 * 1024;
 
-function getValidationErrors() {
-  return [];
+function getValidationErrors(event) {
+  const errors = [];
+  if (ALLOWED_IMAGE_TYPES.indexOf(event.imageType) < 0) {
+    errors.push('imageType must be png or jpg');
+  }
+
+  const fileSizeBytes = event.fileSizeBytes;
+  if (!fileSizeBytes) {
+    errors.push('fileSizeBytes must be specified');
+  } else {
+    const fileSizeBytesValue = parseInt(fileSizeBytes, 10);
+    if (isNaN(fileSizeBytesValue) || fileSizeBytesValue < 0) {
+      errors.push('fileSizeBytes must be a positive integer');
+    } else if (fileSizeBytesValue > MAX_FILE_SIZE_BYTES) {
+      errors.push('fileSizeBytes cannot exceed 5MB');
+    }
+  }
+
+  return errors;
 }
 
 module.exports = (event, context, done) => {
+  console.log(`Event: ${event}`);
   const errors = getValidationErrors(event);
   if (errors.length > 0) {
     done(new Error(`[BadRequest] Validation errors: ${errors.join(',')}`));
+    return;
   }
+
+  console.log('validation done');
 
   const id = uuid();
   const filename = `${path.join(S3_FOLDER_PREFIX, 'orig', id)}.${event.imageType}`;
@@ -22,6 +45,10 @@ module.exports = (event, context, done) => {
     Key: filename,
   };
 
+  console.log('params building done');
   const s3 = new AWS.S3();
-  s3.getSignedUrl(params, (err, url) => done(err, { id, url }));
+  s3.getSignedUrl('putObject', params, (err, url) => {
+    console.log('signed url done');
+    done(err, { id, url });
+  });
 };
--- backend/test/get_upload_url/index.js
+++ backend/test/get_upload_url/index.js
@@ -6,7 +6,7 @@ const proxyquire = require('proxyquire');
 
 function s3Stub() {
   return {
-    getSignedUrl: (params, cb) =>
+    getSignedUrl: (op, params, cb) =>
       cb(null, `https://example.com/${params.Bucket}/${params.Key}`),
   };
 }
@@ -17,21 +17,47 @@ describe('Upload Image', () => {
   const awsStub = { S3: s3Stub };
   const uuid = () => fakeUuid;
 
-  const upload = proxyquire('../../src/get_upload_url', {
+  const getUploadUrl = proxyquire('../../src/get_upload_url', {
     'aws-sdk': awsStub,
     'uuid/v4': uuid,
   });
 
   it('should upload image', (done) => {
-    const event = { imageType: 'png' };
+    const event = { imageType: 'png', fileSizeBytes: 3 * 1024 * 1024 };
 
-    upload(event, {}, (err, response) => {
+    getUploadUrl(event, {}, (err, response) => {
       assert.equal(err, null);
       assert.deepEqual(response, {
         id: fakeUuid,
         url: `https://example.com/mipmapper/images/orig/${fakeUuid}.png`,
       });
       done();
+    });
+  });
+
+  it('fails when image type is invalid', (done) => {
+    const event = { imageType: 'bmp', fileSizeBytes: 3 * 1024 * 1024 };
+
+    getUploadUrl(event, {}, (err) => {
+      assert.deepEqual(err.message, '[BadRequest] Validation errors: imageType must be png or jpg');
+      done();
+    });
+  });
+
+  [
+    ['fails when fileSizeBytes is missing', null, 'fileSizeBytes must be specified'],
+    ['fails when fileSizeBytes is not an integer', 'foo', 'fileSizeBytes must be a positive integer'],
+    ['fails when fileSizeBytes is less than zero', -100, 'fileSizeBytes must be a positive integer'],
+    ['fails when fileSizeBytes is greater than max allowed', 6 * 1024 * 1024, 'fileSizeBytes cannot exceed 5MB'],
+  ].forEach(([testCase, value, expectedError]) => {
+    it(testCase, (done) => {
+      const event = { imageType: 'jpg', fileSizeBytes: value };
+
+      getUploadUrl(event, {}, (err) => {
+        assert.notEqual(null, err);
+        assert.equal(err.message, `[BadRequest] Validation errors: ${expectedError}`);
+        done();
+      });
     });
   });
 });
